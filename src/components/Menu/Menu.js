1816d36da2b2250a0684bef09929be2fd1025b90
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-19 14:06:38 -0700

    Add icons for news categories.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,4 +1,11 @@
 import React from 'react';
+
+import LocalImage from '../../icons/local.svg';
+import TechnologyImage from '../../icons/technology.svg';
+import EntertainmentImage from '../../icons/entertainment.svg';
+import ScienceImage from '../../icons/science.svg';
+import HealthImage from '../../icons/health.svg';
+
 import './Menu.css';
 
 const Menu = ({ changeNewsCategory, newsState }) => {
@@ -9,11 +16,25 @@ const Menu = ({ changeNewsCategory, newsState }) => {
         the <span>news</span>
       </h1>
       <div className='menu-side-bar'>
-        <h2 onClick={() => changeNewsCategory(local)}>Local News</h2>
-        <h2 onClick={() => changeNewsCategory(technology)}>Technology</h2>
-        <h2 onClick={() => changeNewsCategory(entertainment)}>Entertainment</h2>
-        <h2 onClick={() => changeNewsCategory(science)}>Science</h2>
-        <h2 onClick={() => changeNewsCategory(health)}>Health</h2>
+        <h2 onClick={() => changeNewsCategory(local)}>
+          <img className='icon-image' src={LocalImage} /> <h3>Local News</h3>
+        </h2>
+        <h2 onClick={() => changeNewsCategory(technology)}>
+          <img className='icon-image' src={TechnologyImage} />
+          <h3>Technology</h3>
+        </h2>
+        <h2 onClick={() => changeNewsCategory(entertainment)}>
+          <img className='icon-image' src={EntertainmentImage} />
+          <h3>Entertainment</h3>
+        </h2>
+        <h2 onClick={() => changeNewsCategory(science)}>
+          <img className='icon-image' src={ScienceImage} />
+          <h3>Science</h3>
+        </h2>
+        <h2 onClick={() => changeNewsCategory(health)}>
+          <img className='icon-image' src={HealthImage} />
+          <h3>Health</h3>
+        </h2>
       </div>
     </nav>
   );
