f7618478cf3d5f53ae45550c2455fe546b68bc3b
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-19 13:29:00 -0700

    Update to HTML tags and styling for individual news articles.

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './NewsArticle.css';
 
-const NewsArticle = ({ description, headline, img, url }) => {
+const NewsArticle = ({ description, headline, image, url }) => {
   return (
-    <div>
-      <h1>{headline}</h1>
-      <img src={img} />
+    <div className='news-article'>
+      <h3>{headline}</h3>
+      <img src={image} alt={headline} />
       <p>{description}</p>
-      <p>{url}</p>
+      <a href={url}>Link to Article</a>
     </div>
   );
 };
