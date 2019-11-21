50cb23fa92c437f419823bda8bec8e10a87d4f5b
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-21 16:09:22 -0700

    Add a unit test for searchNews query through a real user input.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -84,4 +84,48 @@ describe('App', () => {
 
     expect(wrapper.state('selectedNews')).toEqual(expected);
   });
+
+  it('should run searchNews and update the state based on userInput and filter through to only display appropriate news', () => {
+    const mockState = [
+      {
+        id: 1,
+        headline:
+          'Unfixable iOS Device Exploit Is the Latest Apple Security Upheaval',
+        img:
+          'https://media.wired.com/photos/5d8e600f34cab0000861c48d/master/w_2560%2Cc_limit/security_jailbreak_MADEPD.jpg',
+        description:
+          "Any iPhone device from 2011 to 2017 could soon be jailbroken, thanks to an underlying flaw that there's no way to patch.",
+        url: 'https://www.wired.com/story/ios-exploit-jailbreak-iphone-ipad/'
+      },
+      {
+        id: 2,
+        headline:
+          'SpaceX’s orbital Starship prototype comes together ahead of update event',
+        img:
+          'https://techcrunch.com/wp-content/uploads/2019/09/EFfmFEEU8AA1SmA.jpeg?w=1390&crop=1',
+        description:
+          'SpaceX  has completed joining the bottom and top halves of the Starship orbital-scale prototype it’s building in Boca Chica, Texas.',
+        url:
+          'https://techcrunch.com/2019/09/27/spacexs-orbital-starship-prototype-comes-together-ahead-of-update-event/'
+      }
+    ];
+    const mockState2 = [
+      {
+        id: 1,
+        headline:
+          'Unfixable iOS Device Exploit Is the Latest Apple Security Upheaval',
+        img:
+          'https://media.wired.com/photos/5d8e600f34cab0000861c48d/master/w_2560%2Cc_limit/security_jailbreak_MADEPD.jpg',
+        description:
+          "Any iPhone device from 2011 to 2017 could soon be jailbroken, thanks to an underlying flaw that there's no way to patch.",
+        url: 'https://www.wired.com/story/ios-exploit-jailbreak-iphone-ipad/'
+      }
+    ];
+    const expected = mockState2;
+
+    wrapper.setState({ selectedNews: mockState, searchedNews: mockState });
+    expect(wrapper.state('searchedNews')).toEqual(mockState);
+    wrapper.instance().searchNews('Apple');
+    expect(wrapper.state('selectedNews')).toEqual(expected);
+  });
 });
