3f3d48f192c657b1a620ec3fd163923b56a1a9f9
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-21 16:54:08 -0700

    Get started and setup on async testing.  The test is skipped for right now.  Will probably move to an apiCall to run more testing.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+import { fetchNews } from './App';
+
 describe('App', () => {
   let wrapper;
 
@@ -127,5 +129,45 @@ describe('App', () => {
     expect(wrapper.state('searchedNews')).toEqual(mockState);
     wrapper.instance().searchNews('Apple');
     expect(wrapper.state('selectedNews')).toEqual(expected);
+  });
+
+  it.skip('should call the fetch with the correct URL upon mounting', () => {
+    const mockNewsData = [
+      {
+        id: 1,
+        headline:
+          'Unfixable iOS Device Exploit Is the Latest Apple Security Upheaval',
+        img:
+          'https://media.wired.com/photos/5d8e600f34cab0000861c48d/master/w_2560%2Cc_limit/security_jailbreak_MADEPD.jpg',
+        description:
+          "Any iPhone device from 2011 to 2017 could soon be jailbroken, thanks to an underlying flaw that there's no way to patch.",
+        url: 'https://www.wired.com/story/ios-exploit-jailbreak-iphone-ipad/'
+      },
+      {
+        id: 2,
+        headline:
+          'SpaceX’s orbital Starship prototype comes together ahead of update event',
+        img:
+          'https://techcrunch.com/wp-content/uploads/2019/09/EFfmFEEU8AA1SmA.jpeg?w=1390&crop=1',
+        description:
+          'SpaceX  has completed joining the bottom and top halves of the Starship orbital-scale prototype it’s building in Boca Chica, Texas.',
+        url:
+          'https://techcrunch.com/2019/09/27/spacexs-orbital-starship-prototype-comes-together-ahead-of-update-event/'
+      }
+    ];
+    const mockResponse = {
+      selectedNews: mockNewsData
+    };
+
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockResponse)
+      });
+    });
+
+    expect(window.fetch).toHaveBeenCalledWith(
+      'https://whats-new-api.herokuapp.com/api/v1/news'
+    );
   });
 });
