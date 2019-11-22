6e78d54358ef2840a00d60227e8823bf6e7c51b2
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-21 22:49:23 -0700

    Make minor improvements for all styling across all Components.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -17,23 +17,24 @@ const Menu = ({ changeNewsCategory, newsState }) => {
       </h1>
       <div className='menu-side-bar'>
         <h2 onClick={() => changeNewsCategory(local)}>
-          <img className='icon-image' src={LocalImage} /> <h3>Local News</h3>
+          <img className='icon-image' src={LocalImage} alt='icon' />{' '}
+          <p className='category-label'>Local News</p>
         </h2>
         <h2 onClick={() => changeNewsCategory(technology)}>
-          <img className='icon-image' src={TechnologyImage} />
-          <h3>Technology</h3>
+          <img className='icon-image' src={TechnologyImage} alt='icon' />
+          <p className='category-label'>Technology</p>
         </h2>
         <h2 onClick={() => changeNewsCategory(entertainment)}>
-          <img className='icon-image' src={EntertainmentImage} />
-          <h3>Entertainment</h3>
+          <img className='icon-image' src={EntertainmentImage} alt='icon' />
+          <p className='category-label'>Entertainment</p>
         </h2>
         <h2 onClick={() => changeNewsCategory(science)}>
-          <img className='icon-image' src={ScienceImage} />
-          <h3>Science</h3>
+          <img className='icon-image' src={ScienceImage} alt='icon' />
+          <p className='category-label'>Science</p>
         </h2>
         <h2 onClick={() => changeNewsCategory(health)}>
-          <img className='icon-image' src={HealthImage} />
-          <h3>Health</h3>
+          <img className='icon-image' src={HealthImage} alt='icon' />
+          <p className='category-label'>Health</p>
         </h2>
       </div>
     </nav>
