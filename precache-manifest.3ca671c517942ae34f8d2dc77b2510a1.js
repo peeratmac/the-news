5569a39b8adead2a2f23c470524efd1007f2127f
Peerat Sukcharoenyingyong <peeratmac@icloud.com>
2019-11-22 15:15:51 -0700

    Updates

---
+++ precache-manifest.3ca671c517942ae34f8d2dc77b2510a1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124fa65f3bfb93a76925d02031927e4b",
+    "url": "/the-news/index.html"
+  },
+  {
+    "revision": "633e35bc0a06df9b453e",
+    "url": "/the-news/static/css/main.c7ca6bf9.chunk.css"
+  },
+  {
+    "revision": "5d6269db32cecb3a3fbd",
+    "url": "/the-news/static/js/2.a526c86e.chunk.js"
+  },
+  {
+    "revision": "633e35bc0a06df9b453e",
+    "url": "/the-news/static/js/main.a328d312.chunk.js"
+  },
+  {
+    "revision": "ef31fdcb6d1f1227639e",
+    "url": "/the-news/static/js/runtime-main.fce1a3a8.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/the-news/static/media/entertainment.cb90c5bd.svg"
+  },
+  {
+    "revision": "3603c3d96edacbed783609177f4f1bf5",
+    "url": "/the-news/static/media/health.3603c3d9.svg"
+  },
+  {
+    "revision": "2ab11f96b216ca03e984f5a1c87019ee",
+    "url": "/the-news/static/media/local.2ab11f96.svg"
+  },
+  {
+    "revision": "19048a686fe0ccc990a67c27a35e1cc7",
+    "url": "/the-news/static/media/science.19048a68.svg"
+  },
+  {
+    "revision": "2d3f44a1bd21333b9c03d2672719144b",
+    "url": "/the-news/static/media/technology.2d3f44a1.svg"
+  }
+]);
